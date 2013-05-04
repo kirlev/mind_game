925ee1f75db04fbde14fbd66d47148f1e8a5c608
Nir Le <kirlev@gmail.com>
2013-05-04 16:37:28 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MindGame::Application.config.secret_token = '3ee38faa04ecaea56f69a0500ae70d67d157f3c0a20c584278b32af2554141e7c79589e4bdebe6d95bd958eb581aa15193adf7f18ad1db5d1df7e604be09392b'
